8f8c5f4621ad45738f15cf67cf8436f532da57dc
Jack <jrdevleague13@Juniors-iMac-75.local>
2016-10-08 12:00:07 -1000

    ...

--- js/app.js
+++ js/app.js
@@ -9,14 +9,32 @@ function addPoint(){
 
 function change(){
 	if (points == 100) {
-		document.getElementById("winbox").innerHTML = "You get a Pen";
-		console.log(Apple)
-	}else if (points == 200) {
 		document.getElementById("winbox").innerHTML = "You get a Apple Pen";
-	}else if (points == 300) {
+		document.getElementById("imagebox").innerHTML = "<img src="pens/apple.jpg">";
+	}else if (points == 150) {
+		document.getElementById("winbox").innerHTML = "You get a Watermelon Pen";
+		document.getElementById("imagebox").innerHTML = "<img src="pens/watermelon.jpg">"
+	}else if (points == 200) {
+		document.getElementById("winbox").innerHTML = "You get a Orange Pen";
+		document.getElementById("imagebox").innerHTML = "<img src="pens/orange.jpg">"
+	}else if (points == 250) {
 		document.getElementById("winbox").innerHTML = "You get a Pineapple Pen";
-	}else if (points == 500) {
+		document.getElementById("imagebox").innerHTML = "<img src="pens/pineapple.jpg">"
+	}else if (points == 300) {
+		document.getElementById("winbox").innerHTML = "You get a Strawberry Pen";
+		document.getElementById("imagebox").innerHTML = "<img src="pens/strawberry.jpg">"
+	}else if (points == 350) {
+		document.getElementById("winbox").innerHTML = "You get a Raspberry Pen";
+		document.getElementById("imagebox").innerHTML = "<img src="pens/raspberry.jpg">"
+	}else if (points == 400) {
+		document.getElementById("winbox").innerHTML = "You get a Pepper Pen";
+		document.getElementById("imagebox").innerHTML = "<img src="pens/pepper.jpg">"
+	}else if (points == 450) {
+		document.getElementById("winbox").innerHTML = "You get a Banana Pen";
+		document.getElementById("imagebox").innerHTML = "<img src="pens/banana.jpg">"
+	}else if (points == 1000) {
 		document.getElementById("winbox").innerHTML = "You get a Pen Pineapple Apple Pen";
+		document.getElementById("imagebox").innerHTML = "<img src="pens/ppap.jpg">"
 	}
 }
 
