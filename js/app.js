6687ebbf9739b70c025a1c574add269c77d8719d
Jack <jrdevleague13@Juniors-iMac-75.local>
2016-10-08 11:49:29 -1000

    ...

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-var points;
+var points = 0;
 
-addPoint(){
-	
+function addPoint(){
+	points = points+1; 
+	document.getElementById("points").innerHTML = points;
+	console.log("test");
+	change()
 }
+
+function change(){
+	if (points == 100) {
+		document.getElementById("winbox").innerHTML = "You get a Pen";
+		console.log(Apple)
+	}else if (points == 200) {
+		document.getElementById("winbox").innerHTML = "You get a Apple Pen";
+	}else if (points == 300) {
+		document.getElementById("winbox").innerHTML = "You get a Pineapple Pen";
+	}else if (points == 500) {
+		document.getElementById("winbox").innerHTML = "You get a Pen Pineapple Apple Pen";
+	}
+}
+
