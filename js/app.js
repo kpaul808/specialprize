37307fcff15759a51f1f6e0d681ae26eca683172
Rusty33 <RustyChainzii@gmail.com>
2016-10-15 09:37:21 -1000

    ...

--- js/app.js
+++ js/app.js
@@ -10,74 +10,74 @@ function press(){
 function change(){
 	if (points == 10) {
 		document.getElementById("winbox").innerHTML = "You get a Apple Pen";
-		document.getElementById("imagebox").innerHTML = "<img src='pens/apple.jpg'>";
+		document.getElementById("imagebox").innerHTML = "<img src='pens/apple.png'>";
 		var apen = document.createElement("IMG");
 		console.log("apen")
-		apen.setAttribute("src", "pens/apple.jpg")
+		apen.setAttribute("src", "pens/apple.png")
 		apen.style.width = "50px";
 		document.getElementById("InventoryPics").appendChild(apen)
 	}else if (points == 50) {
 		document.getElementById("winbox").innerHTML = "You get a Watermelon Pen";
-		document.getElementById("imagebox").innerHTML = "<img src='pens/watermelon.jpg'>"
+		document.getElementById("imagebox").innerHTML = "<img src='pens/watermelon.png'>"
 		var wpen = document.createElement("IMG");
 		console.log("wpen")
-		wpen.setAttribute("src", "pens/watermelon.jpg")
+		wpen.setAttribute("src", "pens/watermelon.png")
 		wpen.style.width = "50px";
 		document.getElementById("InventoryPics").appendChild(wpen)
 	}else if (points == 100) {
 		document.getElementById("winbox").innerHTML = "You get a Orange Pen";
-		document.getElementById("imagebox").innerHTML = "<img src='pens/orange.jpg'>"
+		document.getElementById("imagebox").innerHTML = "<img src='pens/orange.png'>"
 		var Open = document.createElement("IMG");
 		console.log("Open")
-		Open.setAttribute("src", "pens/Orange.jpg")
+		Open.setAttribute("src", "pens/Orange.png")
 		Open.style.width = "50px";
 		document.getElementById("InventoryPics").appendChild(Open)
 	}else if (points == 150) {
 		document.getElementById("winbox").innerHTML = "You get a Pineapple Pen";
-		document.getElementById("imagebox").innerHTML = "<img src='pens/pineapple.jpg'>"
+		document.getElementById("imagebox").innerHTML = "<img src='pens/pineapple.png'>"
 		var ppen = document.createElement("IMG");
 		console.log("ppen")
-		ppen.setAttribute("src", "pens/pineapple.jpg")
+		ppen.setAttribute("src", "pens/pineapple.png")
 		ppen.style.width = "50px";
 		document.getElementById("InventoryPics").appendChild(ppen)
 	}else if (points == 200) {
 		document.getElementById("winbox").innerHTML = "You get a Strawberry Pen";
-		document.getElementById("imagebox").innerHTML = "<img src='pens/strawberry.jpg'>"
+		document.getElementById("imagebox").innerHTML = "<img src='pens/strawberry.png'>"
 		var spen = document.createElement("IMG");
 		console.log("spen")
-		spen.setAttribute("src", "pens/strawberry.jpg")
+		spen.setAttribute("src", "pens/strawberry.png")
 		spen.style.width = "50px";
 		document.getElementById("InventoryPics").appendChild(spen)
 	}else if (points == 250) {
 		document.getElementById("winbox").innerHTML = "You get a Raspberry Pen";
-		document.getElementById("imagebox").innerHTML = "<img src='pens/raspberry.jpg'>"
+		document.getElementById("imagebox").innerHTML = "<img src='pens/raspberry.png'>"
 		var rpen = document.createElement("IMG");
 		console.log("rpen")
-		rpen.setAttribute("src", "pens/raspberry.jpg")
+		rpen.setAttribute("src", "pens/raspberry.png")
 		rpen.style.width = "50px";
 		document.getElementById("InventoryPics").appendChild(rpen)
 	}else if (points == 300) {
 		document.getElementById("winbox").innerHTML = "You get a Pepper Pen";
-		document.getElementById("imagebox").innerHTML = "<img src='pens/pepper.jpg'>"
+		document.getElementById("imagebox").innerHTML = "<img src='pens/pepper.png'>"
 		var peppen = document.createElement("IMG");
 		console.log("peppen")
-		peppen.setAttribute("src", "pens/pepper.jpg")
+		peppen.setAttribute("src", "pens/pepper.png")
 		peppen.style.width = "50px";
 		document.getElementById("InventoryPics").appendChild(peppen)
 	}else if (points == 350) {
 		document.getElementById("winbox").innerHTML = "You get a Banana Pen";
-		document.getElementById("imagebox").innerHTML = "<img src='pens/banana.jpg'>"
+		document.getElementById("imagebox").innerHTML = "<img src='pens/banana.png'>"
 		var bpen = document.createElement("IMG");
 		console.log("bpen")
-		bpen.setAttribute("src", "pens/banana.jpg")
+		bpen.setAttribute("src", "pens/banana.png")
 		bpen.style.width = "50px";
 		document.getElementById("InventoryPics").appendChild(bpen)
 	}else if (points == 1000) {
 		document.getElementById("winbox").innerHTML = "You get a Pen Pineapple Apple Pen";
-		document.getElementById("imagebox").innerHTML = "<img src='pens/ppap.jpg'>"
+		document.getElementById("imagebox").innerHTML = "<img src='pens/ppap.png'>"
 		var ppappen = document.createElement("IMG");
 		console.log("ppappen")
-		ppappen.setAttribute("src", "pens/ppap.jpg")
+		ppappen.setAttribute("src", "pens/ppap.png")
 		ppappen.style.width = "100vw";
 		document.getElementById("InventoryPics").appendChild(ppappen)
 		celebrate()
